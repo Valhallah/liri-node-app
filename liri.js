394b77a28f898614a18a87f3ae7f3e2b9e3c519d
ValHall <Valynhall@gmail.com>
2017-04-04 16:35:46 -0400

    added omdb, spotify, and fs functions

--- liri.js
+++ liri.js
@@ -1,5 +1,74 @@
 var inquirer = require('inquirer');
 
-var twitter = require('twiiter');
-var omdb = require('omdb');
+var twitter = require('twitter');
+var omdbApi = require('omdb-client');
 var spotify = require('spotify');
+var fs = require('fs');
+
+//node liri.js my-tweets
+processArgs(process.argv);
+
+function processArgs(args) {
+  var command = args[2];
+
+
+  if(command === "spotify-this-song"){
+    var song = args[3];
+    if (song == "" || song == null) {
+      song = "The Sign";
+    }
+    spotify.search({ type:"track", query:'track:"'+song+'"' }, function(err, data) {
+      //console.log(data);
+      console.log("");
+      for(i = 0; i < data.tracks.items.length && i<1; i++){
+        console.log("Artist:" + " " + data.tracks.items[i].album.artists[0].name);
+        console.log("Album:" + " " + data.tracks.items[i].album.name);
+        console.log("Song Name:" + " " + data.tracks.items[i].name);
+        console.log("Preview Link:" + " " + data.tracks.items[i].preview_url);
+        console.log("");
+      }
+      if ( err ) {
+          console.log('Error occurred: ' + err);
+          return;
+      }
+    });
+  }
+
+  if(command === "movie-this"){
+    var movie = args[3];
+    if (movie == "" || movie == null) {
+      movie = "Mr. Nobody";
+    }
+    var params = {
+      title: movie,
+      type: "movie",
+      incTomatoes: true,
+      plot: "short"
+  }
+  omdbApi.get(params, function(err, data) {
+    console.log("Title:" + " " + data.Title);
+    console.log("Year:" + " " + data.Year);
+    console.log("Actors:" + " " + data.Actors);
+    console.log("Plot:" + " " + data.Plot);
+    console.log("Country:" + " " + data.Country);
+    console.log("Language:" + " " + data.Language);
+    console.log("Rotten Tomatoes Rating:" + " " + data.tomatoRating);
+
+      if(err) {
+          return console.error(err);
+      }
+    });
+  }
+
+  if(command === "do-what-it-says"){
+    fs.readFile("random.txt", "utf8", function(error, data) {
+      var dataArr = ["node","liri.js"];
+      //console.log(data);
+      var myvariable = data.split(",");
+      //console.log(myvariable);
+      dataArr = dataArr.concat(myvariable);
+      //console.log(dataArr);
+      processArgs(dataArr);
+    });
+  }
+}
